e98593befed9e9e661e5e8d687d0d4bb5d31961a
LahiruSasanka2 <lahirusasanka2015@gmail.com>
2019-03-08 07:39:37 +0530

    Hibernate

---
+++ src/lk/ijse/dep/app/entity/OrderDetail.java
@@ -0,0 +1,82 @@
+package lk.ijse.dep.app.entity;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+
+@Entity
+public class OrderDetail extends SuperEntity{
+
+    @EmbeddedId
+    private OrderDetailPK orderDetailPK;
+    private int qty;
+    private double unitPrice;
+
+    @JoinColumn(name = "orderId",referencedColumnName = "oId",insertable = false,updatable = false)
+    private Order order;
+    @JoinColumn(name = "itemCode",referencedColumnName = "itemId",insertable = false,updatable = false)
+    private Item item;
+
+    public OrderDetail() {
+    }
+
+    public OrderDetail(OrderDetailPK orderDetailPK, int qty, double unitPrice, Order order, Item item) {
+        this.orderDetailPK = orderDetailPK;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+        this.order = order;
+        this.item = item;
+    }
+
+    public OrderDetailPK getOrderDetailPK() {
+        return orderDetailPK;
+    }
+
+    public void setOrderDetailPK(OrderDetailPK orderDetailPK) {
+        this.orderDetailPK = orderDetailPK;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDetail{" +
+                "orderDetailPK=" + orderDetailPK +
+                ", qty=" + qty +
+                ", unitPrice=" + unitPrice +
+                ", order=" + order +
+                ", item=" + item +
+                '}';
+    }
+}
